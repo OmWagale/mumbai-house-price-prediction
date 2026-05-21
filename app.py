80401476f8735865f558b8cb8196f9c5086b24ab
Om Wagale <omwagale.07@gmail.com>
2026-05-21 22:07:57 +0530

    Prediction of price

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request
 import pickle
+import pandas as pd
 
 app = Flask(__name__)
 
+# Load model
 model = pickle.load(open('model/house_price_model.pkl', 'rb'))
+
+# Load training columns
+columns = pickle.load(open('model/columns.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
@@ -13,16 +19,38 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
 
+    # User Inputs
     area = float(request.form['area'])
     bhk = int(request.form['bhk'])
     bathroom = int(request.form['bathroom'])
     locality = request.form['locality']
 
-    prediction = "Prediction Coming Soon"
+    # Create empty dataframe
+    input_data = pd.DataFrame(columns=columns)
+
+    # Add one row
+    input_data.loc[0] = 0
+
+    # Fill main features
+    input_data['area'] = area
+    input_data['bedroom_num'] = bhk
+    input_data['bathroom_num'] = bathroom
+
+    # Fill locality column
+    locality_column = 'locality_' + locality
+
+    if locality_column in input_data.columns:
+        input_data[locality_column] = 1
+
+    # Prediction
+    prediction = model.predict(input_data)[0]
+
+    # Convert to lakhs
+    prediction_lakh = round(prediction / 100000, 2)
 
     return render_template(
         'index.html',
-        prediction_text=prediction
+        prediction_text=f"Estimated Price: ₹ {prediction_lakh} Lakhs"
     )
 
 
