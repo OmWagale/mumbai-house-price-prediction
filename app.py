b42134948072eca0e156d740d5c4020e04ecac68
Om Wagale <omwagale.07@gmail.com>
2026-05-21 21:56:54 +0530

    Add model.pkl

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pickle
 
 app = Flask(__name__)
+
+model = pickle.load(open('model/house_price_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    area = float(request.form['area'])
+    bhk = int(request.form['bhk'])
+    bathroom = int(request.form['bathroom'])
+    locality = request.form['locality']
+
+    prediction = "Prediction Coming Soon"
+
+    return render_template(
+        'index.html',
+        prediction_text=prediction
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
