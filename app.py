e8021c879ba17d740b579b78ea8fedb5268971aa
Om Wagale <omwagale.07@gmail.com>
2026-05-22 17:52:39 +0530

    Added extra locality

--- app.py
+++ app.py
@@ -7,19 +7,31 @@ app = Flask(__name__)
 # Load model
 model = pickle.load(open('model/house_price_model.pkl', 'rb'))
 
-# Load training columns
+# Load columns
 columns = pickle.load(open('model/columns.pkl', 'rb'))
+
+# Get locality names
+localities = []
+
+for col in columns:
+    if col.startswith('locality_'):
+        localities.append(col.replace('locality_', ''))
+
+localities.sort()
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        localities=localities
+    )
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
 
-    # User Inputs
+    # Get user inputs
     area = float(request.form['area'])
     bhk = int(request.form['bhk'])
     bathroom = int(request.form['bathroom'])
@@ -28,21 +40,21 @@ def predict():
     # Create empty dataframe
     input_data = pd.DataFrame(columns=columns)
 
-    # Add one row
+    # Create one row with all 0
     input_data.loc[0] = 0
 
-    # Fill main features
+    # Fill basic features
     input_data['area'] = area
     input_data['bedroom_num'] = bhk
     input_data['bathroom_num'] = bathroom
 
-    # Fill locality column
+    # Set selected locality
     locality_column = 'locality_' + locality
 
     if locality_column in input_data.columns:
         input_data[locality_column] = 1
 
-    # Prediction
+    # Predict
     prediction = model.predict(input_data)[0]
 
     # Convert to lakhs
@@ -50,7 +62,8 @@ def predict():
 
     return render_template(
         'index.html',
-        prediction_text=f"Estimated Price: ₹ {prediction_lakh} Lakhs"
+        prediction_text=f"Estimated Price: ₹ {prediction_lakh} Lakhs",
+        localities=localities
     )
 
 
