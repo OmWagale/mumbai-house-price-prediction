cbf35764e1093ae822ff6538ed62dbcf86439da3
Om Wagale <omwagale.07@gmail.com>
2026-05-22 17:56:20 +0530

    Converted lakhs into crores in prediction price

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Load model
+# Load trained model
 model = pickle.load(open('model/house_price_model.pkl', 'rb'))
 
 # Load columns
 columns = pickle.load(open('model/columns.pkl', 'rb'))
 
-# Get locality names
+# Extract localities
 localities = []
 
 for col in columns:
@@ -22,6 +22,7 @@ localities.sort()
 
 @app.route('/')
 def home():
+
     return render_template(
         'index.html',
         localities=localities
@@ -31,38 +32,38 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
 
-    # Get user inputs
+    # Get form data
     area = float(request.form['area'])
     bhk = int(request.form['bhk'])
     bathroom = int(request.form['bathroom'])
     locality = request.form['locality']
 
-    # Create empty dataframe
+    # Create dataframe with all columns
     input_data = pd.DataFrame(columns=columns)
 
-    # Create one row with all 0
+    # Fill all values with 0
     input_data.loc[0] = 0
 
-    # Fill basic features
+    # Add user inputs
     input_data['area'] = area
     input_data['bedroom_num'] = bhk
     input_data['bathroom_num'] = bathroom
 
-    # Set selected locality
+    # Handle locality
     locality_column = 'locality_' + locality
 
     if locality_column in input_data.columns:
         input_data[locality_column] = 1
 
-    # Predict
+    # Predict price
     prediction = model.predict(input_data)[0]
 
-    # Convert to lakhs
-    prediction_lakh = round(prediction / 100000, 2)
+    # Convert to Crores
+    prediction_cr = round(prediction / 10000000, 2)
 
     return render_template(
         'index.html',
-        prediction_text=f"Estimated Price: ₹ {prediction_lakh} Lakhs",
+        prediction_text=f"Estimated Price: ₹ {prediction_cr} Cr",
         localities=localities
     )
 
